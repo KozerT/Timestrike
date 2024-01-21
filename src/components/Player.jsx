dff015c3a89904aa54114c240263ed117fcb08dc
KozerT <kozer.vizastaff@gmail.com>
2024-01-21 15:09:01 +0100

    feat: Added useRef hook to Player component for dynamic name addition and input field clearing

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 const Player = () => {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+
+  const handleClick = () => {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  };
+
   return (
     <div id="content">
       <section id="player">
-        <h2>Welcome </h2>
+        <h2>Welcome {enteredPlayerName ?? "user"}</h2>
         <p>
-          <input type="text" className="player__input" />
-          <button className="player__button">Set Name</button>
+          <input type="text" className="player__input" ref={playerName} />
+          <button className="player__button" onClick={handleClick}>
+            Add Name
+          </button>
         </p>
       </section>
     </div>
