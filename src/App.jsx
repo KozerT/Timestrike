97dd30137f8bda2a1dffb9f4cff16469e43e632d
KozerT <kozer.vizastaff@gmail.com>
2024-01-21 22:05:53 +0100

    feat: Enhanced dialog window UX with score calculation

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div id="challenges">
         <TimerChallenge title="Easy" targetTime={1} />
         <TimerChallenge title="Not Easy" targetTime={5} />
-        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Tough" targetTime={10} />
         <TimerChallenge title="Pros only" targetTime={15} />
       </div>
     </>
